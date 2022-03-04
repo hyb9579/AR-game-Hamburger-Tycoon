d1404d5b34628e6465d451b24c3e54e079ce162c
hyb <>
2022-03-04 12:35:30 +0900

    add comment in PlayUIManager.cs

--- Assets/Scripts/PlayUIManager.cs
+++ Assets/Scripts/PlayUIManager.cs
@@ -5,83 +5,107 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+// PlayScene의 UI를 관리, 주방을 배치하면 그때부터 스크립트가 활성화된다
+// 하지만, UI는 씬이 시작한 순간부터 존재하는데, 이를 관리하는 스트립트또한 함께 활성화 되는 것이 맞는것으로 보인다
 public class PlayUIManager : MonoBehaviour
 {
+    // 랭킹에 관련된 Rank 객체 참조
     [SerializeField]
     private Rank rank;
 
+    // 게임 경과 시간 표시
     [SerializeField]
     private Text timer;
 
     [SerializeField]
     private GameManager gameManager;
 
+    // 게임 플레이 중 스페셜 오더 클리어시에 표시할 별 참조
     [SerializeField]
     private GameObject[] stars;
 
+    // 게임 클리어 후 표시할 별 묶음 참조
     [SerializeField]
     private GameObject[] starsInClearPanel;
 
+    // 오더 내용 표시 (말풍선)
     [SerializeField]
     private Text customerOrderText;
 
+    // 오더별 남은시간 표시 바
     [SerializeField]
     private Scrollbar orderTimer;
 
+    // 게임 완료시 나오는 패널
     [SerializeField]
     private GameObject clearPanel;
 
+    // 스페셜 오더 클리어 갯수만큼 게임 완료시 나오는 별
     [SerializeField]
     private GameObject starSet;
 
+    // 리워드 및 랭킹 등록을 위한 정보 입력 패널
     [SerializeField]
     private GameObject rewardPanel;
 
+    // 랭킹 정보를 안내하는 패널
     [SerializeField]
     private GameObject rankPanel;
 
+    // 클리어 시간 표시
     [SerializeField]
     private Text endTimeText;
 
+    // 클리어한 오더 수
     [SerializeField]
     private Text customerCNT;
 
+    // 리워드 및 랭킹을 위한 플레이어 이름 입력
     [SerializeField]
     private InputField playerName;
 
+    // 리워드 및 랭킹을 위한 전화번호 입력
     [SerializeField]
     private InputField phoneNumber;
 
+    // 랭킹에 표시할 플레이어 정보 레이아웃
     [SerializeField]
     private GameObject playerInfo;
 
+    // 랭킹에 표시할 플레이어 레이아웃을 배치할 UI
     [SerializeField]
     private GameObject playerInfoParent;
 
+    // 리워드 수령을 위한 정보 입력 패널로 가는 버튼
     [SerializeField]
     private GameObject rewardRegisterButton;
 
+    // 재료를 쌓는 버튼
     [SerializeField]
     private GameObject placementButton;
 
+    // 오더 및 오더 만족/불만족에 따른 말풍선 표시를 위해 참조하는 객체들
     public GameObject customerOrder;
-
     public GameObject customerSat;
-
     public GameObject customerUnSat;
-
     public GameObject customerWrong;
 
+    // 영어로된 메뉴와 재료를 한국어로 바꿔주기 위한 딕셔너리
     Dictionary<string, string> menuE2K;
-
     Dictionary<string, string> ingreE2K;
 
     [SerializeField]
     private Text DebugLog;
 
+    // 클리어시 나오는 패널이 active인지 아닌지 확인하는 변수
+    // 따로 이런 변수를 둘 필요 없이 클리어패널을 가져와서 상태를 확인하는게 맞을것 같다
     private bool isClearPanelOn = false;
 
+    // 게임 플레이타임
     public float time = 0;
+    
+    // 변수 초기화
+    // 굳이 Awake()에서 해야할 초기화인지 확인할것
     private void Awake()
     {
         menuE2K = new Dictionary<string, string>()
@@ -113,11 +137,14 @@ public class PlayUIManager : MonoBehaviour
         };
     }
 
+    // PlayerUIManager 스크립트가 활성화 되면 새로운 오더를 만든다
+    // 하지만 이 스크립트는 UI만 다루는 목적으로 작성한 스크립트인데 이 코드가 여기에 있는건 맞지 않는것 같다
     private void OnEnable()
     {
         gameManager.NewOrder();
     }
 
+    // 게임 상황에 따라 여러 UI들을 변경시킴
     private void Update()
     {
         if (gameManager.isEnd)
@@ -155,11 +182,18 @@ public class PlayUIManager : MonoBehaviour
         orderTimer.size = GameManager.order.timer / gameManager.orderTime;
     }
 
+    // 스페셜 오더 클리어시 표시되는 별 갯수 추가
+    /*
+        PlayerUIManager 스크립트는 UI를 관리하는데, 다른 객체 및 스크립트로 부터 정보를 얻어서 UI를 변경하기도하고 
+        다른 객체 및 스크립트에서 PlayerUIManager 를 참조해 UI를 변경하기도 하는데,
+        UI를 바꾸는 방법 위 두가지 방법을 함께 사용하는 것이 좋은지, 하나로 통일하는 것이 좋은지 확인 필요
+    */
     public void AddStar()
     {
         stars[gameManager.specialOrderClearCnt - 1].SetActive(true);
     }
 
+    // 클리어 패널의 별을 스페셜 오더를 클리어 한 만큼 활성화
     private void SetStarsInClearPanel()
     {
         starSet.SetActive(true);
@@ -170,6 +204,7 @@ public class PlayUIManager : MonoBehaviour
         }
     }
 
+    // 오더 내용 텍스트 말풍선에 표시하기
     public void OrderInCol()
     {
         string inText;
@@ -187,11 +222,15 @@ public class PlayUIManager : MonoBehaviour
         customerOrderText.text = inText;
     }
 
+    // 홈버튼 클릭시 씬 이동
     public void OnHomeButton()
     {
         SceneManager.LoadScene("GameMenuScene");
     }
 
+    // 랭킹버튼 클릭시 랭킹 데이터 불러오기
+    // 프리팹으로 저장된 랭킹 표시 레이아웃을 가져온뒤에
+    // 랭킹 데이터를 가져와 배치
     public void OnRankingButton()
     {
         clearPanel.SetActive(false);
@@ -218,17 +257,20 @@ public class PlayUIManager : MonoBehaviour
         }
     }
 
+    // 재도전 버튼 클릭시 씬 다시 시작
     public void OnRetryButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    // 리워드 버튼 클릭스 리워드 UI 활성화
     public void OnRewardButton()
     {
         clearPanel.SetActive(false);
         rewardPanel.SetActive(true);
     }
 
+    // 나가기 버튼 클릭시 클리어 패널 활성화
     public void OnExitButton()
     {
         rewardPanel.SetActive(false);
@@ -236,6 +278,9 @@ public class PlayUIManager : MonoBehaviour
         clearPanel.SetActive(true);
     }
 
+    // 리워드 정보 입력후 정보 등록 (리워드 수령) 버튼을 누르면 실행
+    // 입력한 정보를 데이터에 저장하고 랭킹창 표시
+    // onRankingButton()으로 코드 줄일 수 있을것 같다
     public void OnRewardRegister()
     {
         string playerName = this.playerName.text;
